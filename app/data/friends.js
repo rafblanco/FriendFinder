bb895327913072173eb4483e7f1aaf7a60795a36
Rafael Blanco <rafaelblanco@Rafaels-MBP-2.fios-router.home>
2018-12-06 10:07:50 -0500

    Setup basic environment and layout of the backend javascript and frontend html

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        name: "Jon",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Daenerys",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]     
+    },
+    {
+        name: "Tyrion",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]  
+    },
+    {
+        name: "Rob",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]  
+    },
+    {
+        name: "Hound",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]  
+    },
+    {
+        name: "Cersei",
+        photo: "",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]  
+    }
+]
+
+module.exports = friends;
