340f3d00803ca8718dace862d3846325dda7a9e4
Rafael Blanco <rafaelblanco@Rafaels-MBP-2.fios-router.home>
2018-12-09 23:23:00 -0500

    Final changes to the back end api logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = [
     {
         name: "Jon",
-        photo: "",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwin7rmxspTfAhXim-AKHQGxDmcQjRx6BAgBEAU&url=https%3A%2F%2Fwww.hbo.com%2Fgame-of-thrones%2Fcast-and-crew%2Fjon-snow&psig=AOvVaw0TTYVGd67NZ__rvY9XSE-n&ust=1544501442556933",
         scores: [
             5,
             1,
             4,
-            4,
+            2,
             5,
             1,
             2,
@@ -17,82 +17,82 @@ var friends = [
     },
     {
         name: "Daenerys",
-        photo: "",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/daenarys-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
         scores: [
-            5,
+            2,
+            1,
+            3,
+            2,
+            2,
             1,
             4,
-            4,
-            5,
-            1,
             2,
             5,
-            4,
             1
         ]     
     },
     {
         name: "Tyrion",
-        photo: "",
+        photo: "https://cdn.legalcheek.com/wp-content/uploads/2017/07/lead1-13.jpg",
         scores: [
-            5,
-            1,
             4,
-            4,
-            5,
-            1,
+            3,
             2,
+            1,
             5,
+            3,
+            2,
+            4,
             4,
             1
         ]  
     },
     {
         name: "Rob",
-        photo: "",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/robert-stark-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
         scores: [
-            5,
+            2,
             1,
-            4,
-            4,
+            1,
+            3,
             5,
             1,
             2,
-            5,
-            4,
-            1
+            2,
+            1,
+            3
         ]  
     },
     {
         name: "Hound",
-        photo: "",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi72N3WspTfAhXDc98KHRlqDm0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.hollywoodreporter.com%2Flive-feed%2Fweek-game-thrones-clegane-brothers-901594&psig=AOvVaw0H8CyxqGl0HDzk_lYpRo-R&ust=1544501518334418",
         scores: [
             5,
             1,
             4,
-            4,
+            3,
             5,
+            4,
+            2,
             1,
             2,
-            5,
-            4,
-            1
+            4
         ]  
     },
     {
         name: "Cersei",
-        photo: "",
+        photo: "https://pixel.nymag.com/imgs/daily/vulture/2017/07/14/14-cersei-lannister.w700.h700.jpg",
         scores: [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
+            1,
+            1,
+            3,
             4,
-            1
+            1,
+            5,
+            2,
+            1,
+            2
         ]  
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,23 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req, res){
-        friendsData.push(req.body);
-    })
+        //friendsData.push(req.body);
+        var userScore = req.body.scores;
+        var totalDiff = []; 
+
+        for(var i = 0; i < friendsData.length; i++){
+            var currentCheck = friendsData[i].scores;
+            var friendDiff = 0;            
+            for(var j = 0; j < currentCheck.length; j++){
+                var diff = Math.abs(currentCheck[j] - parseInt(userScore[j]));
+                friendDiff = friendDiff + diff;
+            }
+            totalDiff.push(friendDiff);
+            friendDiff = 0;
+        }
+        console.log(totalDiff)
+        var k = totalDiff.indexOf(Math.min(...totalDiff));
+        console.log(friendsData[k]);
+        res.json(friendsData[k]);
+    });
 }
